730ba00dddd5c2e800a2eaa478e9d6bda85495f8
JoelFx <G00408551@atu.ie>
2025-04-25 14:12:35 +0100

    Add stubbed ChallengesPage: card list and Create/Join buttons

--- src/app/pages/challenges/challenges.page.ts
+++ src/app/pages/challenges/challenges.page.ts
@@ -1,20 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+
+export interface Challenge {
+  id: string;
+  title: string;
+  description: string;
+  start: string;
+  end: string;
+}
 
 @Component({
   selector: 'app-challenges',
+  standalone: true,
+  imports: [IonicModule, CommonModule],
   templateUrl: './challenges.page.html',
   styleUrls: ['./challenges.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ChallengesPage implements OnInit {
-
-  constructor() { }
+  challenges: Challenge[] = [];
 
   ngOnInit() {
+    this.loadChallenges();
   }
 
+  loadChallenges() {
+    this.challenges = [
+      {
+        id: '1',
+        title: 'Monthly Cycling Distance Challenge',
+        description: 'Ride a total of 1,250 km',
+        start: 'Jan 1, 2022',
+        end: 'Jan 31, 2022'
+      },
+      {
+        id: '2',
+        title: 'Weekly Running Goal',
+        description: 'Run 20 miles in a week',
+        start: 'Apr 20, 2025',
+        end: 'Apr 26, 2025'
+      }
+    ];
+  }
+
+  onCreate() {
+    console.log('Create a new group challenge');
+  }
+
+  onJoin(ch: Challenge) {
+    console.log('Join challenge', ch);
+  }
 }
