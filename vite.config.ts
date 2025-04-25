19da1ba5f1ff740bbbf92094ec7d99a81a5bae71
JoelFx <G00408551@atu.ie>
2025-04-25 16:29:30 +0100

    Add vite.config.ts with default import from @analogjs/vite-plugin-angular and exclude Ionic components

---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+// vite.config.ts
+import { defineConfig } from 'vite';
+import createAngularPlugin from '@analogjs/vite-plugin-angular';
+
+export default defineConfig({
+  plugins: [
+    createAngularPlugin()
+  ],
+  optimizeDeps: {
+    exclude: [
+      '@ionic/angular',
+      '@ionic/core',
+      // Ionic Web Component entrypoints:
+      'ion-app',
+      'ion-router-outlet',
+      'ion-tabs',
+      'ion-tab-bar',
+      'ion-tab-button',
+      'ion-icon',
+      'ion-button',
+      'ion-item',
+      'ion-list',
+      'ion-card',
+      'ion-card-header',
+      'ion-card-content',
+      'ion-card-footer',
+      'ion-avatar',
+      'ion-label',
+      'ion-toolbar',
+      'ion-title'
+    ]
+  }
+});
