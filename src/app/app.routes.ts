5e73aa2dfc5285d04f4ab4a357a5d81c067297da
JoelFx <G00408551@atu.ie>
2025-04-25 14:48:59 +0100

    Load standalone TabsPage in root routing so tabs and pages render

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,28 +1,23 @@
-import { Routes } from '@angular/router';
+// src/app/app.routes.ts
 
-export const routes: Routes = [
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'tabs/home',
+    pathMatch: 'full'
   },
   {
-    path: 'home',
-    loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
-  },
-  {
-    path: 'maps',
-    loadComponent: () => import('./pages/maps/maps.page').then( m => m.MapsPage)
-  },
-  {
-    path: 'record',
-    loadComponent: () => import('./pages/record/record.page').then( m => m.RecordPage)
-  },
-  {
-    path: 'challenges',
-    loadComponent: () => import('./pages/challenges/challenges.page').then( m => m.ChallengesPage)
-  },
-  {
-    path: 'you',
-    loadComponent: () => import('./pages/you/you.page').then( m => m.YouPage)
-  },
+    path: 'tabs',
+    loadComponent: () =>
+      import('./tabs/tabs.page').then(m => m.TabsPage)
+  }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutesModule {}
